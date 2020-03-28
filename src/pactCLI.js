b99601e15ebf5be6b10b5ba724afee3fbe77cfb3
Miles Crighton <miles.crighton@gmail.com>
2020-03-28 12:30:36 +0000

    Pre-branch working build

--- src/pactCLI.js
+++ src/pactCLI.js
@@ -6,55 +6,55 @@ const inquirer = require('./inquirer');
 const apiComms = require('./pactAPIInterface');
 const helpers = require('./helpers');
 
-const CoffeeMaker = require('./CoffeeMaker');
+// const CoffeeMaker = require('./CoffeeMaker');
 
-// async function main() {
-//     try {
-//         //@todo: Let this main function be a infinite loop for inquirer until exited
-//         helpers.displayHeader();
-
-//         const tokenDecimal = await apiComms.authenticate();
-//         const tokenBASE64 = helpers.toBASE64(tokenDecimal);
-
-//         helpers.displayHeader();
-//         console.log(chalk.green('Authentication successful.'));
-
-//         const userData = await apiComms.getUserData(tokenBASE64);
-//         const orderID = userData.start.order_ids[0];
-//         //const coffeeData = await apiComms.getCoffeeData(tokenBASE64)
-//         //const available = helpers.filterAvailableCoffees(coffeeData)
-//         helpers.displayOrderStatus(userData);
-
-//         const answers = await inquirer.askOptions();
-//         if (answers.date) {
-//             const requestedDate = helpers.handleDateInput(answers.date);
-//             await apiComms.changeDate(tokenBASE64, orderID, requestedDate);
-//         }
-//         //if (answers.coffee) await apiComms.changeCoffee(tokenBASE64, orderID, item, answers.coffee)
-
-//         apiComms.deauthenticate(tokenBASE64);
-//     } catch (error) {
-//         console.log(error);
-//     }
-// }
-
-async function mainTest() {
+async function main() {
     try {
+        //@todo: Let this main function be a infinite loop for inquirer until exited
         helpers.displayHeader();
-        const CoffeeMakerInstance = new CoffeeMaker();
-        await CoffeeMakerInstance.authenticate();
+
+        const tokenDecimal = await apiComms.authenticate();
+        const tokenBASE64 = helpers.toBASE64(tokenDecimal);
+
         helpers.displayHeader();
-        await CoffeeMakerInstance.getUserData();
-        console.log(CoffeeMakerInstance);
-        // while (true) {
-        //     helpers.displayHeader();
-        //     await CoffeeServiceInstance.displayOrderStatus();
-        //     await CoffeeServiceInstance.shutdown();
-        //     break;
-        // }
-    } catch (e) {
-        console.log(e);
+        console.log(chalk.green('Authentication successful.'));
+
+        const userData = await apiComms.getUserData(tokenBASE64);
+        const orderID = userData.start.order_ids[0];
+        //const coffeeData = await apiComms.getCoffeeData(tokenBASE64)
+        //const available = helpers.filterAvailableCoffees(coffeeData)
+        helpers.displayOrderStatus(userData);
+
+        const answers = await inquirer.askOptions();
+        if (answers.date) {
+            const requestedDate = helpers.handleDateInput(answers.date);
+            await apiComms.changeDate(tokenBASE64, orderID, requestedDate);
+        }
+        //if (answers.coffee) await apiComms.changeCoffee(tokenBASE64, orderID, item, answers.coffee)
+
+        apiComms.deauthenticate(tokenBASE64);
+    } catch (error) {
+        console.log(error);
     }
 }
 
-mainTest();
+// async function mainTest() {
+//     try {
+//         helpers.displayHeader();
+//         const CoffeeMakerInstance = new CoffeeMaker();
+//         await CoffeeMakerInstance.authenticate();
+//         helpers.displayHeader();
+//         await CoffeeMakerInstance.getUserData();
+//         console.log(CoffeeMakerInstance);
+//         // while (true) {
+//         //     helpers.displayHeader();
+//         //     await CoffeeServiceInstance.displayOrderStatus();
+//         //     await CoffeeServiceInstance.shutdown();
+//         //     break;
+//         // }
+//     } catch (e) {
+//         console.log(e);
+//     }
+// }
+
+main();
