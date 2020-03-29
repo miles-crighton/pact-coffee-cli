462216981dbd294318d6f0866a6a8602a360d4f7
Miles Crighton <miles.crighton@gmail.com>
2020-03-29 21:34:21 +0100

    Tidied up main function

--- src/pactCLI.js
+++ src/pactCLI.js
@@ -3,42 +3,10 @@ require('dotenv').config({ path: path.join(__dirname, '..', '.env') });
 const chalk = require('chalk');
 
 const inquirer = require('./inquirer');
-const apiComms = require('./pactAPIInterface');
 const helpers = require('./helpers');
 
 const CoffeeMaker = require('./CoffeeMaker');
 
-async function oldMain() {
-    try {
-        //@todo: Let this main function be a infinite loop for inquirer until exited
-        helpers.displayHeader();
-
-        const tokenDecimal = await apiComms.authenticate();
-        const tokenBASE64 = helpers.toBASE64(tokenDecimal);
-
-        helpers.displayHeader();
-        console.log(chalk.green('Authentication successful.'));
-
-        const userData = await apiComms.getUserData(tokenBASE64);
-        const orderID = userData.start.order_ids[0];
-        //const coffeeData = await apiComms.getCoffeeData(tokenBASE64)
-        //const available = helpers.filterAvailableCoffees(coffeeData)
-        helpers.displayOrderStatus(userData);
-
-        const answers = await inquirer.askOptions();
-        if (answers.date) {
-            const requestedDate = helpers.handleDateInput(answers.date);
-            await apiComms.changeDate(tokenBASE64, orderID, requestedDate);
-        }
-        //if (answers.coffee) await apiComms.changeCoffee(tokenBASE64, orderID, item, answers.coffee)
-
-        apiComms.deauthenticate(tokenBASE64);
-    } catch (error) {
-        console.log(error);
-    }
-}
-
-//@todo wire up each function to inquirer responses
 async function main() {
     try {
         helpers.displayHeader();
@@ -48,7 +16,7 @@ async function main() {
         await CoffeeMakerInstance.getUserData();
         await CoffeeMakerInstance.displayOrderStatus();
 
-        let answer;
+        let answers;
         let running = true;
         while (running) {
             answers = await inquirer.askOptions();
